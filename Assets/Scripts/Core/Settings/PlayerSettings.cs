e990ca4475a1c4f92c354d1c1c45a6a385686809
fizix <4luxxx@gmail.com>
2023-10-19 15:12:23 +0100

    Added animation and movement data to player settings

--- Assets/Scripts/Core/Settings/PlayerSettings.cs
+++ Assets/Scripts/Core/Settings/PlayerSettings.cs
@@ -1,9 +1,22 @@
 using UnityEngine;
 
 namespace Frogalypse.Settings {
-	[CreateAssetMenu(fileName = "PlayerSettings", menuName = "Frogalypse/Settings/Player")]
+	// Disabled becauseonly one asset instance is needed
+	//[CreateAssetMenu(fileName = "PlayerSettings", menuName = "Frogalypse/Settings/Player")]
 	public class PlayerSettings : ScriptableObject {
+		[Header("Movement")]
+		public float groundMoveSpeed = 2f;
+		public float airMoveSpeed = 3f;
+		public float jumpForce = 5f;
+
+		[Header("Tether")]
 		public float maxGrappleDistance;
 		public ContactFilter2D grappleContactFilter;
+
+		[Header("Animation")]
+		public float firingSpeed = 5f;
+		public float reelingSpeed = 4f;
+		public AnimationCurve firingSpeedCurve;
+		public AnimationCurve reelingSpeedCurve;
 	}
 }
