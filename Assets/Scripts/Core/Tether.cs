f4403d4fdf9e353499af58a56929f5667337deeb
fizix <4luxxx@gmail.com>
2023-10-19 15:17:15 +0100

    set up tether states

--- Assets/Scripts/Core/Tether.cs
+++ Assets/Scripts/Core/Tether.cs
@@ -1,3 +1,4 @@
+using System;
 using Frogalypse.Input;
 
 using SideFX;
@@ -10,8 +11,18 @@ namespace Frogalypse {
 		[SerializeField] private InputReader _input;
 
 		// Components
+		// Fields
+		private Vector3 _targetPosition = Vector3.zero;
+		[SerializeField] private TetherState _state = TetherState.Disabled;
 
 		private void Awake() => _lineRenderer = GetComponent<LineRenderer>();
+		private enum TetherState : byte {
+			Ready = 0,
+			Firing = 1,
+			Attached = 2,
+			Reeling = 3,
+			Disabled = 4,
+		}
 
 		private void OnEnable() {
 			if (_input == null) {
@@ -29,6 +40,27 @@ namespace Frogalypse {
 			_input.GrappleEvent -= CreateTether;
 		}
 
-		private void CreateTether() { }
+		private void CreateTether() {
+			switch (_state) {
+				case TetherState.Ready:
+					Debug.Log("Tether is ready, firing!");
+					_state = TetherState.Firing;
+					return;
+				case TetherState.Firing:
+					Debug.LogWarning($"Tether already launched, can't launch until reeled in", this);
+					return;
+				case TetherState.Attached:
+					Debug.Log("Already tethered to an object, ignoring CreateTether", this);
+					return;
+				case TetherState.Reeling:
+					Debug.Log("Tether is being reeled in, can't create new tether just yet");
+					return;
+				case TetherState.Disabled:
+					Debug.LogWarning("Failed to create tether: tether is disabled", this);
+					return;
+				default:
+					throw new ArgumentOutOfRangeException(nameof(_state), _state, "Tether state value not recognised");
+			};
+		}
 	}
 }
