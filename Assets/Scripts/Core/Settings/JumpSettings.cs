6c2dce7a59ae553d6de032205da1a85f769f47dc
fizix <4luxxx@gmail.com>
2023-10-31 16:41:56 +0000

    Add JumpSettings

---
+++ Assets/Scripts/Core/Settings/JumpSettings.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+namespace Frogalypse.Settings {
+	[CreateAssetMenu(fileName = "ActorJumpSettings", menuName = "Frogalypse/Settings/Actors/Jump")]
+	public class JumpSettings : MonoBehaviour {
+		public float JumpForce;
+		public float MaxJumpHoldTime;
+		[Range(0f, 1f)] public float MinGravityScale;
+	}
+}
--- Assets/Scripts/Core/Settings/PlayerSettings.cs
+++ Assets/Scripts/Core/Settings/PlayerSettings.cs
@@ -5,6 +5,7 @@ namespace Frogalypse.Settings {
 	//[CreateAssetMenu(fileName = "PlayerSettings", menuName = "Frogalypse/Settings/Player")]
 	public class PlayerSettings : ScriptableObject {
 		public ActorMoveSettings MoveSettings;
+		public JumpSettings JumpSettings;
 
 		[Header("Tether")]
 		public float maxGrappleDistance;
