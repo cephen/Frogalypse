18a77b05acdfd554a653520a5320b5d8321ca048
fizix <4luxxx@gmail.com>
2023-11-07 15:56:39 +0000

    Added ScriptableObjects for storing data about levels

---
+++ Assets/Scripts/Core/Levels/LevelData.cs
@@ -0,0 +1,16 @@
+﻿using SideFX.Scenes;
+
+using UnityEngine;
+
+namespace Frogalypse.Levels {
+	[CreateAssetMenu(fileName = "Level x", menuName = "Frogalypse/Level Data")]
+	internal class LevelData : ScriptableObject {
+		[SerializeField] private GameplaySceneSO _scene;
+		[SerializeField] private bool _isCompleted = false;
+
+		internal GameplaySceneSO Scene => _scene;
+		internal bool IsCompleted => _isCompleted;
+
+		internal void MarkComplete() => _isCompleted = true;
+	}
+}
