a48e498ce0c8e0a937a7a0c91152bd76f0b33b59
fizix <4luxxx@gmail.com>
2023-11-07 15:58:58 +0000

    Add custom UI Elements for selecting levels

---
+++ Assets/Scripts/Core/UI/LevelFrame.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.UIElements;
+
+namespace Frogalypse.UI {
+	public class LevelFrame : Button {
+		public new class UxmlFactory : UxmlFactory<LevelFrame> { }
+		public void Init(int levelNumber, bool complete) {
+			style.backgroundColor = complete ? Color.white : Color.clear;
+			text = $"{levelNumber:00}";
+		}
+
+		// Custom UI controls need a default constructor
+		public LevelFrame() {
+			AddToClassList("level-frame");
+			Init(0, false);
+		}
+	}
+}
---
+++ Assets/Scripts/Core/UI/LevelSelector.cs
@@ -0,0 +1,39 @@
+﻿using DG.Tweening;
+
+using Frogalypse.Levels;
+
+using UnityEngine.UIElements;
+
+namespace Frogalypse.UI {
+	public class LevelSelector : VisualElement {
+		public new class UxmlFactory : UxmlFactory<LevelSelector> { }
+
+		private const float _activeOpacity = 0.6f;
+
+		public LevelSelector() { }
+
+		internal void Populate(LevelDB levels) {
+			for (int i = 0 ; i < levels.Count ; i++) {
+				LevelData data = levels[i];
+				LevelFrame frame = new();
+				frame.Init(i + 1, data.IsCompleted);
+				Add(frame);
+			}
+		}
+
+		internal void Show() =>
+			DOTween.To(
+				getter: () => style.opacity.value,
+				setter: (float x) => style.opacity = x,
+				endValue: _activeOpacity,
+				duration: 1f);
+
+		internal void Hide() =>
+			DOTween.To(
+				getter: () => style.opacity.value,
+				setter: (float x) => style.opacity = x,
+				endValue: 0f,
+				duration: 1f);
+
+	}
+}
