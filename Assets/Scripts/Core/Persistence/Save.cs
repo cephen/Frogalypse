a2ca99a7b94c15fe78860c44c2aed4aa609ec3c9
fizix <4luxxx@gmail.com>
2023-11-14 13:26:15 +0000

    Add level records to save

--- Assets/Scripts/Core/Persistence/Save.cs
+++ Assets/Scripts/Core/Persistence/Save.cs
@@ -1,6 +1,9 @@
 using System;
+using System.Collections.Generic;
 
 using Frogalypse.Settings;
+
+using SideFX.Scenes;
 
 using UnityEngine;
 
@@ -8,7 +11,20 @@ namespace Frogalypse.Persistence {
 	[Serializable]
 	internal class Save {
 		public FrogalypseSettings Settings { get; private set; } = FrogalypseSettings.Default();
+		public Dictionary<GameplaySceneSO, LevelRecord> LevelRecords { get; private set; } = new();
 
+		public void SaveRecord(GameplaySceneSO level, LevelRecord newRecord) {
+			if (!LevelRecords.ContainsKey(level))
+				LevelRecords.Add(level, LevelRecord.Default());
+
+			LevelRecord current = LevelRecords[level];
+			LevelRecord next = new() {
+				IsComplete = true,
+				BestTime = newRecord.BestTime.TotalMilliseconds < current.BestTime.TotalMilliseconds ? current.BestTime : newRecord.BestTime,
+			};
+
+			LevelRecords[level] = next;
+		}
 
 		public void SaveSettings(GameSettingsSO settings) {
 			Settings = new FrogalypseSettings {
