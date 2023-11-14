8529109b8e3f32769b963f6ddcf50a2b53d0f282
fizix <4luxxx@gmail.com>
2023-11-14 13:27:36 +0000

    Use save system in LevelDB

--- Assets/Scripts/Core/Levels/LevelDB.cs
+++ Assets/Scripts/Core/Levels/LevelDB.cs
@@ -1,12 +1,41 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+
+using Frogalypse.Persistence;
+
+using SideFX.Scenes;
+
+using UnityEngine;
 
 namespace Frogalypse.Levels {
 	[CreateAssetMenu(fileName = "LevelDB", menuName = "Frogalypse/Databases/Levels")]
 	internal class LevelDB : ScriptableObject {
-		[SerializeField] private LevelData[] _levels;
-
-		public LevelData this[int i] => _levels[i];
+		[SerializeField] private GameplaySceneSO[] _levels;
+		[SerializeField] private SaveSystem _saveSystem;
+		private readonly Dictionary<GameplaySceneSO, LevelRecord> _records = new();
 
 		public int Count => _levels.Length;
+		public GameplaySceneSO this[int i] => _levels[i];
+		public LevelRecord this[GameplaySceneSO level] {
+			get {
+				if (!_records.ContainsKey(level)) {
+					_records.Add(level, LevelRecord.Default());
+				}
+				return _records[level];
+			}
+		}
+
+		private void OnEnable() => _saveSystem.SaveLoadedEvent += OnSaveLoaded;
+		private void OnDisable() => _saveSystem.SaveLoadedEvent -= OnSaveLoaded;
+
+		private void OnSaveLoaded(Save save) {
+			_records.Clear();
+			foreach (GameplaySceneSO level in _levels) {
+				if (save.LevelRecords.TryGetValue(level, out LevelRecord record)) {
+					_records.Add(level, record);
+				} else {
+					_records.Add(level, LevelRecord.Default());
+				}
+			}
+		}
 	}
 }
