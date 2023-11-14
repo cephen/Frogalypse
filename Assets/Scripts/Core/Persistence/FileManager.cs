1ce0a14a29de5f362db9021f3a477c0db8011d46
Ash Villaschi <4luxxx@gmail.com>
2023-11-14 03:51:27 +0000

    Added FileManager class

---
+++ Assets/Scripts/Core/Persistence/FileManager.cs
@@ -0,0 +1,36 @@
+using System;
+using System.IO;
+
+using UnityEngine;
+
+namespace Frogalypse.Persistence {
+	public static class FileManager {
+		public static bool WriteToFile(string fileName, string contents) {
+			var fullPath = Path.Combine(Application.persistentDataPath, fileName);
+
+			try {
+				File.WriteAllText(fullPath, contents);
+				return true;
+			} catch (Exception e) {
+				Debug.LogError($"Failed to write to {fullPath} with exception {e}");
+				return false;
+			}
+		}
+
+		public static bool LoadFromFile(string fileName, out string result) {
+			var fullPath = Path.Combine(Application.persistentDataPath, fileName);
+
+			if (!File.Exists(fullPath))
+				File.WriteAllText(fullPath, string.Empty);
+
+			try {
+				result = File.ReadAllText(fullPath);
+				return true;
+			} catch (Exception e) {
+				Debug.LogError($"Failed to read from {fullPath} with exception {e}");
+				result = string.Empty;
+				return false;
+			}
+		}
+	}
+}
