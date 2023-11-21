a352c56f4824955e54712124100abf3b312c2ce9
fizix <4luxxx@gmail.com>
2023-11-21 16:27:05 +0000

    Add simple stage hazard script

---
+++ Assets/Scripts/Core/Components/Environment/StageHazard.cs
@@ -0,0 +1,14 @@
+using Frogalypse.Components;
+
+using UnityEngine;
+
+namespace Frogalypse {
+	[RequireComponent(typeof(BoxCollider2D))]
+	public class StageHazard : MonoBehaviour {
+		private void OnCollisionEnter2D(Collision2D collision) {
+			if (collision.gameObject.CompareTag("Player") && collision.gameObject.TryGetComponent(out HealthComponent health)) {
+				health.Damage(1);
+			}
+		}
+	}
+}
