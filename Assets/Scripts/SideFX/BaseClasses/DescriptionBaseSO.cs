0192e6169f562db075240d693835bcc7e8618a94
fizix <4luxxx@gmail.com>
2023-11-21 10:10:55 +0000

    Migrate SideFX package to git

--- Assets/Scripts/SideFX/BaseClasses/DescriptionBaseSO.cs
+++
@@ -1,7 +0,0 @@
-using UnityEngine;
-
-namespace SideFX {
-	public class DescriptionBaseSO : ScriptableObject {
-		[SerializeField, TextArea(3, 6)] private string _description;
-	}
-}
--- Assets/Scripts/SideFX/SceneManagement/ScriptableObjects/GameplaySceneSO.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-namespace SideFX.Scenes {
-	/// <summary>
-	/// This class contains Settings specific to Gameplay scenes only
-	/// </summary>
-	[CreateAssetMenu(fileName = "New Gameplay Scene", menuName = "SideFX/Scene Data/Gameplay")]
-	public class GameplaySceneSO : SceneDataSO { }
-}
