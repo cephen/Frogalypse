e55a07b52e69cbe61bf19535c4275f3fb3c40aae
Ash Villaschi <4luxxx@gmail.com>
2023-11-28 22:07:42 +0000

    Add a healthbar widget

---
+++ Assets/Scripts/Core/UI/HUD/HealthbarWidget.cs
@@ -0,0 +1,9 @@
+using UnityEngine.UIElements;
+
+namespace Frogalypse.UI {
+	public class HealthbarWidget : VisualElement {
+		public new class UxmlFactory : UxmlFactory<HealthbarWidget> { }
+
+		public HealthbarWidget() { }
+	}
+}
