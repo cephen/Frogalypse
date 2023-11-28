114600c5e24218fd93ce6ed363221b99e8afeca3
Ash Villaschi <4luxxx@gmail.com>
2023-11-28 23:26:14 +0000

    refactor: use serialized auto properties to cut down file size

--- Assets/Scripts/Core/UI/HUD/HealthbarData.cs
+++ Assets/Scripts/Core/UI/HUD/HealthbarData.cs
@@ -5,20 +5,12 @@ using UnityEngine;
 namespace Frogalypse.UI {
 	[CreateAssetMenu(fileName = "HealthbarData", menuName = "Frogalypse/UI/Data/Healthbar")]
 	public class HealthbarData : ScriptableObject {
-		[SerializeField] private Sprite _emptyLeft;
-		[SerializeField] private Sprite _emptyMid;
-		[SerializeField] private Sprite _emptyRight;
-		[SerializeField] private Sprite _filledLeft;
-		[SerializeField] private Sprite _filledMid;
-		[SerializeField] private Sprite _filledRight;
-		[SerializeField] private HealthData _playerHealthData;
-
-		public Sprite EmptyLeft => _emptyLeft;
-		public Sprite EmptyMid => _emptyMid;
-		public Sprite EmptyRight => _emptyRight;
-		public Sprite FilledLeft => _filledLeft;
-		public Sprite FilledMid => _filledMid;
-		public Sprite FilledRight => _filledRight;
-		public HealthData PlayerHealth => _playerHealthData;
+		[field: SerializeField] public Sprite EmptyLeft { get; private set; }
+		[field: SerializeField] public Sprite EmptyMid { get; private set; }
+		[field: SerializeField] public Sprite EmptyRight { get; private set; }
+		[field: SerializeField] public Sprite FilledLeft { get; private set; }
+		[field: SerializeField] public Sprite FilledMid { get; private set; }
+		[field: SerializeField] public Sprite FilledRight { get; private set; }
+		[field: SerializeField] public HealthData PlayerHealth { get; private set; }
 	}
 }
