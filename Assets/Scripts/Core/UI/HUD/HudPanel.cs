b3414c034850007935cd75ba774d241a6a8f80e1
Ash Villaschi <4luxxx@gmail.com>
2023-11-28 23:02:22 +0000

    Add Healthbar widget data to Hud Panel

--- Assets/Scripts/Core/UI/HUD/HudPanel.cs
+++ Assets/Scripts/Core/UI/HUD/HudPanel.cs
@@ -4,16 +4,22 @@ using UnityEngine.UIElements;
 namespace Frogalypse.UI {
 	[RequireComponent(typeof(UIDocument))]
 	public class HudPanel : MonoBehaviour {
+		[SerializeField] private HealthbarData _healthbarData;
+
 		private UIDocument _document;
 		private VisualElement _root;
 		private HealthbarWidget _healthBar;
 
 		private void Awake() {
+			// TODO: Handle nulls
 			if (TryGetComponent(out UIDocument component)) {
 				_document = component;
 				_root = _document.rootVisualElement;
 				_healthBar = _root.Q<HealthbarWidget>("health-bar");
 			}
+
+			if (_healthBar != null && _healthbarData != null)
+				_healthBar.Init(_healthbarData);
 		}
 	}
 }
