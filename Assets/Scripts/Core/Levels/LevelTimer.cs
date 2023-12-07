05eb9db848bcd43fcb35ad1fc29951ddbae21917
fizix <4luxxx@gmail.com>
2023-12-07 13:25:58 +0000

    Refactor LevelTimer.Elapsed
    return updating value when running and fixed value when stopped

--- Assets/Scripts/Core/Levels/LevelTimer.cs
+++ Assets/Scripts/Core/Levels/LevelTimer.cs
@@ -6,32 +6,36 @@ namespace Frogalypse.Levels {
 		private DateTime _endTime;
 		private State _state;
 
-		internal readonly TimeSpan Elapsed => _endTime - _startTime;
+		internal readonly TimeSpan Elapsed => _state switch {
+			State.Running => DateTime.UtcNow - _startTime,
+			State.Stopped => _endTime - _startTime,
+			_ => throw new ArgumentOutOfRangeException(nameof(_state)),
+		};
 
 		internal LevelTimer(bool startNow) {
 			_startTime = DateTime.UtcNow;
 			_endTime = DateTime.UtcNow;
-			_state = State.Inactive;
+			_state = State.Stopped;
 
 			if (startNow)
 				Start();
 		}
 
 		internal enum State {
-			Active,
-			Inactive,
+			Running,
+			Stopped,
 		}
 
 		internal void Start() {
 			_startTime = DateTime.UtcNow;
-			_state = State.Active;
+			_state = State.Running;
 		}
 
 		internal TimeSpan Finish() {
 			switch (_state) {
-				case State.Active:
-					_endTime = DateTime.Now;
-					_state = State.Inactive;
+				case State.Running:
+					_endTime = DateTime.UtcNow;
+					_state = State.Stopped;
 					return Elapsed;
 				default:
 					return Elapsed;
