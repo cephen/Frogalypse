b7cad0077e86d042efe78dc59ceeb37b899fb67d
fizix <4luxxx@gmail.com>
2023-12-12 11:06:59 +0000

    Refactor health component to use scriptable variables

--- Assets/Scripts/Core/Components/Player/HealthComponent.cs
+++ Assets/Scripts/Core/Components/Player/HealthComponent.cs
@@ -1,19 +1,37 @@
-using Frogalypse.SharedData;
+using System;
+
+using Frogalypse.Data;
 
 using UnityEngine;
 
 namespace Frogalypse.Components {
 	internal class HealthComponent : MonoBehaviour {
-		[SerializeField] private HealthData _healthData;
+		[field: SerializeField] internal ByteVariable MaxHealth { get; private set; }
+		internal byte CurrentHealth { get; private set; }
 
-		private void Awake() => _healthData.Reset();
+		internal event Action Died = delegate { };
+		internal event Action HealthChanged = delegate { };
 
-		private void OnEnable() => _healthData.Died += OnDeath;
+		private void Awake() => CurrentHealth = MaxHealth.Value;
 
-		private void OnDisable() => _healthData.Died -= OnDeath;
+		private void OnEnable() {
+			HealthChanged += OnHealthChanged;
+			Died += OnDeath;
+		}
+
+		private void OnDisable() {
+			HealthChanged -= OnHealthChanged;
+			Died -= OnDeath;
+		}
+
+		public void Damage(byte amount) {
+			CurrentHealth = (byte) Mathf.Max(CurrentHealth - amount, 0);
+			HealthChanged?.Invoke();
+			if (CurrentHealth == 0)
+				Died?.Invoke();
+		}
 
 		private void OnDeath() => Debug.Log($"{name} just died! L + skill issue + ratio");
-
-		public void Damage(byte amount) => _healthData.Damage(amount);
+		private void OnHealthChanged() => Debug.Log($"{name}'s health changed: {CurrentHealth}");
 	}
 }
